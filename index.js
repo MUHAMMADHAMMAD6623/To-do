da700254a6c57a5aa16806c8f2af77712cbdb994
MUHAMMADHAMMAD6623 <142398205+MUHAMMADHAMMAD6623@users.noreply.github.com>
2024-08-10 13:39:54 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const input = document.querySelector('#input')
-    const button = document.querySelector('.add')
-    const list = document.querySelector('#todo-list')
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('#input');
+    const button = document.querySelector('.add');
+    const list = document.querySelector('#todo-list');
 
+    button.addEventListener('click', () => {
+        if (input.value === '') {
+            alert('Add a task');
+        } else {
+            addTodo(input.value);
+            input.value = '';
+            saveData();
+        }
+    });
 
-    button.addEventListener('click',()=>{
-        if(input.value === ''){
-            alert('Add a task')
-        }
-        else{
-            var li = document.createElement('li')
-            li.innerHTML = input.value
-            list.appendChild(li)
-            let del = document.createElement('img')
-            del.src = 'delete.png'
-            del.className='delete'
-            li.appendChild(del)
-            li.addEventListener('click',()=>{
-                li.classList.toggle('checked')
-                saveData();
-            })
-            del.addEventListener('click',()=>{
-                list.removeChild(li)
-                saveData();
-            })
-        }
-        input.value = ''
-        saveData();
-    })
-    function saveData(){
-        localStorage.setItem('Task',list.innerHTML)
+    function addTodo(text) {
+        var li = document.createElement('li');
+        li.innerHTML = text;
+
+        let del = document.createElement('img');
+        del.src = 'delete.png';
+        del.className = 'delete';
+        li.appendChild(del);
+
+        li.addEventListener('click', () => {
+            li.classList.toggle('checked');
+            saveData();
+        }, false);
+
+        del.addEventListener('click', () => {
+            list.removeChild(li);
+            saveData();
+        }, false);
+
+        list.appendChild(li);
     }
-    function getData(){
-        const storedTasks = localStorage.getItem('Task')
-        list.innerHTML = storedTasks
+
+    function saveData() {
+        localStorage.setItem('Task', list.innerHTML);
     }
+
+    function getData() {
+        list.innerHTML = localStorage.getItem('Task');
+        // Reattach event listeners after retrieving data
+        let listItems = list.querySelectorAll('li');
+        listItems.forEach((li) => {
+            li.addEventListener('click', () => {
+                li.classList.toggle('checked');
+                saveData();
+            });
+
+            let del = li.querySelector('.delete');
+            del.addEventListener('click', () => {
+                list.removeChild(li);
+                saveData();
+            });
+        });
+    }
+
     getData();
-})
+});
